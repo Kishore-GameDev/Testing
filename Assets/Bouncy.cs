0336ec798f5edb330e4b4726bb7887893d3e8ec0
Kishore-GameDev <148840060+Kishore-GameDev@users.noreply.github.com>
2023-12-21 14:00:35 +0530

    Update Bouncy.cs

--- Assets/Bouncy.cs
+++ Assets/Bouncy.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class Bouncy : MonoBehaviour
 {
     public Rigidbody rigidBody;
-    public float force;
-    public bool onFloor;
+    public float force, stopDistance;
+    public bool onFloor, reached;
     public Transform point;
+    public Vector3 velocity;
 
     private void Update()
     {
+        velocity = rigidBody.velocity;
         Vector3 player = new Vector3(transform.position.x, 0.5f, transform.position.z);
         Vector3 pointPos = new Vector3(point.position.x, 0.5f, point.position.z);
 
-        if (Vector3.Distance(player, pointPos) > 0.1f)
+        if (Vector3.Distance(player, pointPos) > 0.1f && !reached)
         {
-            if(Vector3.Distance(player, pointPos) < 0.2f)
+            if(Vector3.Distance(player, pointPos) < stopDistance)
             {
                 //transform.position = new Vector3(point.position.x, 1f, point.position.z);
-                rigidBody.useGravity = false;
-                rigidBody.constraints = RigidbodyConstraints.FreezeAll;
+                //rigidBody.useGravity = false;
+                //rigidBody.constraints = RigidbodyConstraints.FreezeAll;
+                reached = true;
+            }
+            else
+            {
+                //rigidBody.useGravity = true;
+                //rigidBody.constraints = RigidbodyConstraints.None;
             }
             if (onFloor)
             {
+                rigidBody.velocity = Vector3.zero;
                 rigidBody.AddForce(Vector3.up * force, ForceMode.Force);
             }
             transform.position = Vector3.MoveTowards(transform.position, point.position, Time.deltaTime * 0.5f);
             Debug.Log("If");
+        }
+        else if(Vector3.Distance(player, pointPos) > stopDistance)
+        {
+            reached = false;
         }
     }
 
